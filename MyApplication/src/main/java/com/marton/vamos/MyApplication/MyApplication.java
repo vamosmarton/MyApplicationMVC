28b41a6a7019d1ca51a771de88b2251b48af64e1
vamosmarton <vammarton@gmail.com>
2023-12-06 20:29:47 +0100

    added GSON API

--- MyApplication/src/main/java/com/marton/vamos/MyApplication/MyApplication.java
+++ MyApplication/src/main/java/com/marton/vamos/MyApplication/MyApplication.java
@@ -1,13 +1,40 @@
 package com.marton.vamos.MyApplication;
 
+import com.google.gson.Gson;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
-public class MyApplication {
+public class MyApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
 	}
 
+
+	// Felülírom a CommandLineRunner run metódusát
+	@Override
+	public void run(String... args) throws Exception {
+
+		// ArrayList<> létrehozása
+		ArrayList<Restaurant> restaurantArrayList = new ArrayList<>();
+
+		// éttermek példányosítás és hozzáadása a listához
+		Restaurant restaurant1 = new Restaurant(1, "Pizza Hut", "Lombard Street");
+		Restaurant restaurant2 = new Restaurant(2, "McDonald's", "U.S. Route 66");
+		Restaurant restaurant3 = new Restaurant(3, "KFC", "Beale Street");
+
+		restaurantArrayList.add(restaurant1);
+		restaurantArrayList.add(restaurant2);
+		restaurantArrayList.add(restaurant3);
+
+		// GSON felhasználásával String kimenet készítése és kiírása
+		Gson gson = new Gson();
+		String jsonString = gson.toJson(restaurantArrayList);
+
+		System.out.println(jsonString);
+	}
 }
